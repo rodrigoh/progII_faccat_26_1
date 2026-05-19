c3f268da0d86dee5b464d8c96501f1a528d2355b
Rodrigo Henrich <rodrigohenrich@faccat.br>
2026-05-18 22:35:48 -0300

    Exemplo static

---
+++ src/aula12/exemplo01/Aluno.java
@@ -0,0 +1,10 @@
+package aula12.exemplo01;
+
+public class Aluno {
+    String nome;
+    float[] notas;
+
+    Aluno(int quant){
+        notas = new float[quant];
+    }
+}
---
+++ src/aula12/exemplo01/Exemplo.java
@@ -0,0 +1,7 @@
+package aula12.exemplo01;
+
+public class Exemplo {
+    static void main() {
+        Aluno a1 = new Aluno(3);
+    }
+}
